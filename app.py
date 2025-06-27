7f07cc41138f4ef9a0015c9ec6e7dff616016ee1
Vaidnyani19 <vaidnyanithakare19@gmail.com>
2025-06-27 14:12:43 +0530

    Fix: Use dynamic port binding for Render

--- app.py
+++ app.py
@@ -60,5 +60,7 @@ def get_match_image(filename):
     return send_file(os.path.join(MATCHES_FOLDER, filename))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
